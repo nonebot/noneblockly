cbf20034ce6832c28c31bd3aa87af74186bd4bf1
mobyw <mobyw66@gmail.com>
2024-09-12 22:42:17 +0800

    :bug: Fix scoll bar

--- packages/app/src/components/ContentCard.vue
+++ packages/app/src/components/ContentCard.vue
@@ -7,25 +7,25 @@
   >
     <v-toolbar density="compact" color="secondary-container">
       <v-tabs v-model="OutputsStore.activeTab" bg-color="secondary-container">
-        <v-tab value="tab-1">教程</v-tab>
-        <v-tab value="tab-2">代码</v-tab>
+        <v-tab value="tab-1"> 教程 </v-tab>
+        <v-tab value="tab-2"> 代码 </v-tab>
       </v-tabs>
     </v-toolbar>
 
     <v-window v-model="OutputsStore.activeTab" class="content-card">
       <v-window-item value="tab-1">
         <v-card flat>
-          <v-card-text>
+          <div class="content-tab">
             <slot name="tab-1" />
-          </v-card-text>
+          </div>
         </v-card>
       </v-window-item>
 
       <v-window-item value="tab-2">
         <v-card flat>
-          <v-card-text>
+          <div class="content-tab">
             <slot name="tab-2" />
-          </v-card-text>
+          </div>
         </v-card>
       </v-window-item>
     </v-window>
@@ -36,13 +36,26 @@
 #content-card-component {
   height: 100%;
   width: 100%;
-  overflow: hidden;
 }
 
 .content-card {
   padding-bottom: 1.4rem;
-  width: 100%;
-  overflow: hidden;
+}
+
+@media (min-width: 768px) {
+  .content-tab {
+    padding: 1rem;
+    max-height: 86vh;
+    overflow: auto;
+  }
+}
+
+@media (max-width: 767px) {
+  .content-tab {
+    padding: 1rem;
+    max-height: 42vh;
+    overflow: auto;
+  }
 }
 </style>
 
